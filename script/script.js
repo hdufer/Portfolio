18585ae96f478861435c434d5e97947533b6b6e2
hdufer <hdufer@student.42.fr>
2019-10-22 18:44:50 +0200

    SkillBar canvas animation done

--- script/script.js
+++ script/script.js
@@ -15,5 +15,30 @@ navLinkAboutMe.onclick = () => scrollToAboutMeSection();
 const canvas = document.getElementById('bar-circle');
 const ctx = canvas.getContext('2d');
 
-ctx.fillStyle = "blue";
-ctx.fillRect(10, 10, 70, 70);
+
+let state = 0.1
+
+function drawCircle(fillColor, strokeColor, percentageText) {
+    window.requestAnimationFrame(function() {drawCircle(fillColor, strokeColor, percentageText)});
+
+    ctx.fillStyle = fillColor;
+    ctx.strokeStyle = strokeColor;
+    ctx.lineWidth = 5;
+
+    if (state < 0.9 * 1.5)
+        state += 0.01;
+    else
+        return;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
+    ctx.arc(50, 50, 35, 1.5 * Math.PI, state * Math.PI, false);
+    ctx.fill();
+    ctx.stroke();
+
+    ctx.font = "20px Montserrat";
+    ctx.fillStyle = "gray"
+    ctx.fillText(percentageText, 30, 55);
+}
+
+drawCircle("white", "red", "90%");
