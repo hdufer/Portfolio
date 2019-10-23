793c02d269f982d95e49b44a58e4cc7ccebe7017
hdufer <hdufer@student.42.fr>
2019-10-23 15:22:53 +0200

    SkillBar animation done when user scroll

--- script/script.js
+++ script/script.js
@@ -12,25 +12,25 @@ navLinkAboutMe.onclick = () => scrollToAboutMeSection();
 
 // Canvas animation for Skills section
 
-const canvas = document.getElementById('bar-circle');
-const ctx = canvas.getContext('2d');
+let canvases = document.getElementsByClassName('skillCanvas');
+console.log(canvases);
 
+let state = 0.1;
 
-let state = 0.1
-
-function drawCircle(fillColor, strokeColor, percentageText) {
-    window.requestAnimationFrame(function() {drawCircle(fillColor, strokeColor, percentageText)});
+function drawCircle(fillColor, strokeColor, percentage, idCanvas) {
+    let ctx = canvases[idCanvas].getContext('2d');
+    window.requestAnimationFrame(function() {drawCircle(fillColor, strokeColor, percentage, idCanvas)});
 
     ctx.fillStyle = fillColor;
     ctx.strokeStyle = strokeColor;
     ctx.lineWidth = 5;
 
-    if (state < 0.9 * 1.5)
+    if (state < percentage * 1.5)
         state += 0.01;
     else
         return;
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvases[idCanvas].width, canvases[idCanvas].height);
     ctx.beginPath();
     ctx.arc(50, 50, 35, 1.5 * Math.PI, state * Math.PI, false);
     ctx.fill();
@@ -38,7 +38,38 @@ function drawCircle(fillColor, strokeColor, percentageText) {
 
     ctx.font = "20px Montserrat";
     ctx.fillStyle = "gray"
-    ctx.fillText(percentageText, 30, 55);
+    ctx.fillText(percentage * 100 + "%", 30, 55);
 }
 
-drawCircle("white", "red", "90%");
+// Scrolling In percentage
+
+let h = document.documentElement, 
+    b = document.body,
+    st = 'scrollTop',
+    sh = 'scrollHeight';
+
+// If page reload on right percentage to drawCircle
+let percentScroll = (h[st]||b[st]) / ((h[sh]||b[sh]) - h.clientHeight) * 100;
+if (percentScroll >= 90)
+{
+        drawCircle("white", "red", 0.9, 0);
+        drawCircle("white", "skyblue", 0.8, 1);
+        drawCircle("white", "lightgreen", 0.8, 2);
+        drawCircle("white", "purple", 0.8, 3);
+}
+
+// Event for the percentage to drawCircle
+document.body.onscroll = () => {
+    percentScroll = (h[st]||b[st]) / ((h[sh]||b[sh]) - h.clientHeight) * 100;
+    if (percentScroll >= 90)
+    {
+        drawCircle("white", "red", 0.9, 0);
+        drawCircle("white", "skyblue", 0.8, 1);
+        drawCircle("white", "lightgreen", 0.8, 2);
+        drawCircle("white", "purple", 0.8, 3);
+    }
+}
+
+
+
+
